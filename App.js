c79d537103060456fc18b169933450a658d07a56
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2021-07-18 11:22:34 +0500

    Added react-native-svg and blue tick

--- App.js
+++ App.js
@@ -1,17 +1,101 @@
-import React from 'react';
-import { Text, View, StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Button, StatusBar, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import TopTabNavigator from './navigators/TopTabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Modal from 'react-native-modal';
+import Tick from './assets/tick.svg';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import button from './sounds/playSoundFunc';
 
-
 const App = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [isWhatsApp, setIsWhatsApp] = useState(true);
+  const [isBusinessWhatsApp, setIsBusinessWhatsApp] = useState(false);
+  const [isYoWhatsApp, setIsYoWhatsApp] = useState(false);
+
+  const toggleModal = () => {
+    button.play((success) => {
+      if (success) {
+        button.stop();
+      }
+    });
+    setModalVisible(!isModalVisible);
+  };
+
+  const handleOptionPress = type => {
+    if (type == 'whatsapp' && !isWhatsApp){
+      setIsWhatsApp(true);
+      setIsBusinessWhatsApp(false);
+      setIsYoWhatsApp(false);
+    } else if (type == 'Bwhatsapp' && !isBusinessWhatsApp){
+      setIsWhatsApp(false);
+      setIsBusinessWhatsApp(true);
+      setIsYoWhatsApp(false);
+    } else if (type == 'Ywhatsapp' && !isYoWhatsApp){
+      setIsWhatsApp(false);
+      setIsBusinessWhatsApp(false);
+      setIsYoWhatsApp(true);
+    } 
+  }
+
   return (
     <>
-      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+      <Modal isVisible={isModalVisible}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ width: '80%', marginBottom: 10, fontWeight: 'bold' }}>
+            <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 17 }}>What You Are Using!</Text>
+          </View>
+          <View style={{ width: '80%', marginBottom: 10 }}>
+
+            <TouchableWithoutFeedback onPress={handleOptionPress.bind(null, 'whatsapp')}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 10 }}>
+                <View style={styles.flex1}>
+                  <Text style={{ color: 'white', textAlign: 'left' }}>WhatsApp</Text>
+                </View>
+                <View style={styles.flex1}>
+                  {isWhatsApp && (
+                    <Tick width={220} height={16} />
+                  )}
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+
+            <TouchableWithoutFeedback onPress={handleOptionPress.bind(null, 'Bwhatsapp')}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 10 }}>
+                <View style={styles.flex1}>
+                  <Text style={{ color: 'white', textAlign: 'left' }}>Business WhatsApp</Text>
+                </View>
+                <View style={styles.flex1}>
+                  {isBusinessWhatsApp && (
+                    <Tick width={220} height={16} />
+                  )}
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+
+            <TouchableWithoutFeedback onPress={handleOptionPress.bind(null, 'Ywhatsapp')}>
+              <View style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 10 }}>
+                <View style={styles.flex1}>
+                  <Text style={{ color: 'white', textAlign: 'left' }}>Yo WhatsApp</Text>
+                </View>
+                <View style={styles.flex1}>
+                  {isYoWhatsApp && (
+                    <Tick width={220} height={16} />
+                  )}
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+
+          </View>
+
+          <View style={{ width: '80%' }}>
+            <Button title="Done" onPress={toggleModal} color={'grey'} />
+          </View>
+        </View>
+      </Modal>
+
+      <StatusBar barStyle={'light-content'} backgroundColor={'#1B1B1B'} />
       <View
         style={styles.header}
       >
@@ -20,7 +104,7 @@ const App = () => {
         </View>
         <View style={styles.settingContainer}>
           <TouchableOpacity onPress={() => {
-            button.play();
+            toggleModal();
           }}>
             <Icon name={'settings'} size={20} color={'white'} />
           </TouchableOpacity>
@@ -54,5 +138,8 @@ const styles = StyleSheet.create({
   settingContainer: {
     marginRight: 15,
     marginTop: 15
+  },
+  flex1: {
+    flex: 1
   }
 })
--- metro.config.js
+++ metro.config.js
@@ -4,14 +4,37 @@
  *
  * @format
  */
+//Default configurations
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+// };
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
